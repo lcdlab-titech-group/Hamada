e5a2cef84e831639bcfc6d9fb685fbcc41b9f4d4
moeh0523 <78546926+moeh0523@users.noreply.github.com>
2022-03-07 18:25:22 +0900

    Add files via upload

--- DrawLine.py
+++ DrawLine.py
@@ -1,30 +1,22 @@
 # -*- coding:utf-8 -*-
 import serial
-import sys
 import threading
-import datetime
 import time
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 from serial.tools import list_ports
-
-
-# "S" command: To activate the specified device B.
-#        S + The number of device B + The address of them
-# "E" command: To correct offset of all device B.
-#        E
-# "V" command: To sepecify delay time for L command.
-#        V + delay time [1 - 999ms]
-
+import sys
 
 class serialCom():
-
+    
     def __init__(self):
         self.ser = serial.Serial()
         self.ser.baoudrate = baudrate
         self.ser.timeout = 2
         self.ser_port= self.select_port()
         self.stop_event = threading.Event()
-        self.recvT = threading.Thread(target=self.recvThread)
+        self.recvT = threading.Thread(target=self.DrawLine)
 
     #COMポート番号は繋ぐたびに代わるので、スクリプト実行のたびに探索
     def select_port(self):
@@ -48,69 +40,67 @@ class serialCom():
         except:
             print("エラー：ポートが開けませんでした。")
             return None
-        
-    def OpenCSV(self):
-        if self.ser_port.isOpen():
-            now = datetime.datetime.now()
-            now_str = str(now).replace(" ","_").replace(":","")    
-            self.fd = open(saveDir +"/"+now_str+".csv",'w')
-        
-    def recvThread(self):
-        print("Thread START")
-        data=""
+
+    def DrawLine(self):
+        plt.ion()#インタラクティブに図を描画
+        figure, ax = plt.subplots(figsize=(8,6))  
+        t = [0] * 100
+        y1 = [500] * 100
+        y2 = [500] * 100
+        li_L, = ax.plot(t, y1, linewidth = 3, label = "Longitudinal-axis")
+        li_V, = ax.plot(t, y2, linewidth = 3, label = "Vertical-axis")
+        plt.ylim(800, 1200)
+        plt.xlabel("Time [s]")
+        plt.ylabel("Sensor data [-]")
+        Time = 0
+        plt.legend()
         while not self.stop_event.is_set():
-            if self.ser_port.inWaiting() > 0:
-                readdata = self.ser_port.readline()
-                now = datetime.datetime.now()
-                t = now.time()
-                nowTime = str(t.hour) + ":" + str(t.minute) + ":" + str(t.second)
-                if readdata != "":
-                    try:
-                        data = readdata.strip().decode('utf-8')
-                        data = data.replace("!", "")
-                        data = data.split(",")
-                        B_Address = int(data[0])
-                        Sensor1 = int(data[1], base=16)
-                        Sensor2 = int(data[2], base=16)
-                        
-                        self.fd.write("%s,%d,%d,%d\n"%(str(now), B_Address, Sensor1, Sensor2))
-                        self.fd.flush()
-                        if debugFlag:
-                            sys.stdout.write("READ:%s,%d,%d,%d\n"%(str(now), B_Address, Sensor1, Sensor2))
+            Time += .1
+           
+            readdata = self.ser.readline()#センサ1の2軸センサ値を読み込む
+            data = readdata.strip().decode('utf-8').replace("!", "").split(",")
+            Sensor1_x = int(data[1], base=16)
+            Sensor1_y = int(data[2], base=16)
 
-                    except Exception as e:
-                        print(u"例外args:", e.args)
-                        print("Received data is invalid.")
-                        print(data)
-                        continue
+            readdata = self.ser.readline()#センサ2の2軸センサ値を読み込む
+            data = readdata.strip().decode('utf-8').replace("!", "").split(",")
+            Sensor2_x = int(data[1], base=16)
+            Sensor2_y = int(data[2], base=16)
 
-              
+#                 X = Sensor1_x + Sensor2_x
+#                 Y = Sensor1_y + Sensor2_y
+
+            t.append(Time)#末尾に足して
+            t.pop(0)#最初を消す
+            y1.append(Sensor1_y)
+            y1.pop(0)
+            y2.append(Sensor2_y)
+            y2.pop(0)
+           
+            li_L.set_xdata(t)#グラフの値更新
+            li_L.set_ydata(y1)
+            li_V.set_xdata(t)
+            li_V.set_ydata(y2)
+
+            plt.xlim(min(t), max(t))
+            plt.ylim(min(min(y1, y2, key=min)) - 50, max(max(y1, y2, key=max)) + 50)
+
+            figure.canvas.draw()
+            figure.canvas.flush_events()
+            time.sleep(.01)#待つ             
+
     def stop(self):
         self.stop_event.set()
         self.recvT.join()
-        self.ser_port.write(b"Q\r")
+        self.ser.write(b"Q\r")
         print("Thread STOPED")
-        print("File saved")
-        self.ser_port.close()
-        self.fd.close()
+        self.ser.close()
         #exit()
-
-
+    
 if __name__ == '__main__':
     
-    
     baudrate = 115200
-    E_Cmd = "E\r"
-    saveDir = r"C:\Users\MOE HAMADA\Documents\pyscripts_402"
-    arguments = sys.argv
-    
-     debugFlag = False
-    if len(arguments) > 1:
-        if arguments[1] == "-v":
-            debugFlag = True 
-   
     configFile = open("config.ini",'r')
-    
     for line in configFile:
         line = line.replace("\n",":")
         items = line.split(":")
@@ -123,24 +113,19 @@ if __name__ == '__main__':
         elif items[0] == "Delay Time":
             V_Cmd = "V"+items[1]+"\r"
             print(V_Cmd)
-      
-    S_Cmd = "S"+'{0:03d}'.format(num) + idList+"\r"
-   
-    
-    
+
+    S_Cmd = "S"+'{0:03d}'.format(num) + idList+"\r"  
     com = serialCom()
-    com.OpenCSV()
     S_Num = int(S_Cmd[1:4])
     com.ser_port.write(bytes(S_Cmd,"utf-8"))
     print("Cmd:",S_Cmd)
-    com.ser_port.write(b"E\r")
-    print("E-command")
+    com.ser_port.write(b"E\r")#E-Command
+    
     while True:
         readdata = com.ser_port.readline()
         data = readdata.decode('utf-8')
         if data != "":      # return = "done"
             break
-  
     
     # thread start!!
     com.recvT.setDaemon(True)   # スレッドをデーモン化
